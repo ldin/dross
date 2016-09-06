a8f08a746600636ff1eb299d900ce5f3e0d20556
ldin <ldinka.alena@gmail.com>
2016-09-06 20:57:38 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://drosselshop/admin/');
+define('HTTP_CATALOG', 'http://drosselshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://drosselshop/admin/');
+define('HTTPS_CATALOG', 'http://drosselshop/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ldinka/projects/drosselshop/www/admin/');
+define('DIR_SYSTEM', '/home/ldinka/projects/drosselshop/www/system/');
+define('DIR_LANGUAGE', '/home/ldinka/projects/drosselshop/www/admin/language/');
+define('DIR_TEMPLATE', '/home/ldinka/projects/drosselshop/www/admin/view/template/');
+define('DIR_CONFIG', '/home/ldinka/projects/drosselshop/www/system/config/');
+define('DIR_IMAGE', '/home/ldinka/projects/drosselshop/www/image/');
+define('DIR_CACHE', '/home/ldinka/projects/drosselshop/www/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/ldinka/projects/drosselshop/www/system/storage/download/');
+define('DIR_LOGS', '/home/ldinka/projects/drosselshop/www/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/ldinka/projects/drosselshop/www/system/storage/modification/');
+define('DIR_UPLOAD', '/home/ldinka/projects/drosselshop/www/system/storage/upload/');
+define('DIR_CATALOG', '/home/ldinka/projects/drosselshop/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root~/pr');
+define('DB_PASSWORD', 'gfhjkm');
+define('DB_DATABASE', 'drosselshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
